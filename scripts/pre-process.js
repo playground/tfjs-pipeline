12c3653ff67a9ac4d22ae2a1dae5ecbe778d7979
Jeff Lu <ljeff@us.ibm.com>
2021-06-10 17:49:55 -0400

    add gitignore

--- scripts/pre-process.js
+++ scripts/pre-process.js
@@ -1,49 +1,59 @@
 #! /usr/bin/env node
-const {rename, readdir, copyFile, existsSync, mkdirSync, readFileSync} = require('fs');
+const {renameSync, readdirSync, copyFileSync, existsSync, mkdirSync, readFileSync, writeFileSync} = require('fs');
 const jsonfile = require('jsonfile');
 const convert = require('xml-js');
+const { Observable, forkJoin } = require('rxjs');
+const cp = require('child_process'),
+exec = cp.exec;
+
 
 const task = process.env.npm_config_task || 'rename_maximo_assets';
 
 let build = {
   rename_maximo_assets: () => {
-    const path = process.env.npm_config_path;
-    if(!path) {
-      build.exit('path is required, provide --path=...');
-    }
-    jsonfile.readFile(`${path}/prop.json`, (err, json) => {
-      if (!err) {
+    return new Observable((observer) => {
+      const path = process.env.npm_config_path;
+      let count = 0;
+      if(!path) {
+        build.exit('path is required, provide --path=...');
+      }
+      let json = jsonfile.readFileSync(`${path}/prop.json`);
+      if (json) {
         let fileInfo = JSON.parse(json['file_prop_info']);
         fileInfo.map((info) => {
-          console.log(info['original_file_name']);
+          // console.log(info['original_file_name']);
           let type = /\.[0-9a-z]{1,5}$/i.exec(info.original_file_name)[0];
           let name = info.original_file_name.replace(type, '');
-          console.log(`${path}/${info._id}${type}`, `${path}/${info.original_file_name}`)
-          console.log(`${path}/${info._id}.xml`, `${path}/${name}.xml`)
-          rename(
-            `${path}/${info._id}${type}`, 
-            `${path}/${info.original_file_name}`,
-            err => console.log(err));
-          rename(
-            `${path}/${info._id}.xml`, 
-            `${path}/${name}.xml`,
-            err => console.log(err));
+          // console.log(`${path}/${info._id}${type}`, `${path}/${info.original_file_name}`)
+          // console.log(`${path}/${info._id}.xml`, `${path}/${name}.xml`)
+          if(existsSync(`${path}/${info._id}.xml`) && existsSync(`${path}/${info._id}${type}`)) {
+            renameSync(
+              `${path}/${info._id}${type}`, 
+              `${path}/${info.original_file_name}`);
+            renameSync(
+              `${path}/${info._id}.xml`, 
+              `${path}/${name}.xml`);
+            count++;
+          }    
         })
+        console.log(`renamed total of ${count} image files and ${count} xml files`)
+        observer.next();
+        observer.complete();
       } else {
-        build.exit(err);
+        build.exit(json);
       }
-    });  
+    });
   },
   partition_dataset: () => {
-    const path = process.env.npm_config_path;
+    return new Observable((observer) => {
+      const path = process.env.npm_config_path;
     if(!path) {
       build.exit('path is required, provide --path=...');
     }
     const train_ratio = process.env.npm_config_ratio || 0.9;
-    readdir(path, (err, files) => {
+    let files = readdirSync(path);
       const assets = files.filter((file) => file.match(/jpg|png|gif|jpeg/i));
       const train_num = parseInt(assets.length * train_ratio);
-      console.log(`total: ${assets.length}, train: ${train_num}, test: ${assets.length-train_num}`);
       if(!existsSync(`${path}/train`)) {
         mkdirSync(`${path}/train`)
       } else {
@@ -55,49 +65,122 @@ let build = {
         build.exit('test directory already exists');
       }
       assets.forEach((asset, i) => {
-        console.log(asset, i)
+        // console.log(asset, i)
         let type = /\.[0-9a-z]{1,5}$/i.exec(asset)[0];
         let name = asset.replace(type, '');
         let target = i < train_num ? 'train' : 'test';
-        copyFile(`${path}/${asset}`, `${path}/${target}/${asset}`, (err) => console.log(err));
-        copyFile(`${path}/${name}.xml`, `${path}/${target}/${name}.xml`, (err) => console.log(err));
+        if(existsSync(`${path}/${name}.xml`) && existsSync(`${path}/${asset}`)) {
+          copyFileSync(`${path}/${asset}`, `${path}/${target}/${asset}`);
+          copyFileSync(`${path}/${name}.xml`, `${path}/${target}/${name}.xml`);
+        }
       })
-    });
+      console.log(`total: ${assets.length}, train: ${train_num}, test: ${assets.length-train_num}`);
+      observer.next();
+      observer.complete();
+    });  
   },
   xml_to_csv: () => {
-    const path = process.env.npm_config_path;
-    const origin = process.env.npm_config_origin;
-    let xmls, xml;
-    if(!path || !origin) {
-      build.exit('path and orgin are required, provide --path=... --origin=...');
-    }
-    ['train', 'test'].forEach((target) => {
-      const dir = `${path}/${target}`;
-      readdir(dir, (err, files) => {
-        xmls = files.filter((file) => file.indexOf('.xml') > 0);
-        xmls.forEach((name, i) => {
-          xml = readFileSync(`${dir}/${name}`);
-          let json = JSON.parse(convert.xml2json(xml, {compact: true, space: 2}));
-          console.log(json.annotation.object)
-          let object = json.annotation.object;
-          try {
-            object.forEach((obj) => {
-              // console.log(obj.name, obj.bndbox)
-            })
-           let csv = '';
-           if(origin.toLowerCase() === 'maximo') {
-             csv = 'filename,width,height,class,xmin,ymin,xmax,ymax\n';
- 
-           }
- 
-          } catch(e) {
-            console.log(json.annotation, name)
-            build.exit(name)
+    return new Observable((observer) => {
+        const path = process.env.npm_config_path;
+      const origin = process.env.npm_config_origin;
+      let xmls, csv = '';
+      let $call = [];
+      if(!path || !origin) {
+        build.exit('path and orgin are required, provide --path=... --origin=...');
+      }
+      ['train', 'test'].forEach((target) => {
+        const dir = `${path}/${target}`;
+        console.log(target, origin)
+        let files = readdirSync(dir);
+          xmls = files.filter((file) => file.indexOf('.xml') > 0);
+          // console.log(xmls)
+          if(origin.toLowerCase() === 'maximo') {
+            $call.push(build.maximo(xmls, dir, path));
           }
-        })
       })
-    })
+      forkJoin($call)
+      .subscribe((data) => {
+        writeFileSync(`${path}/train/labels.csv`, data[0]);
+        writeFileSync(`${path}/test/labels.csv`, data[1]);
+        console.log('generating label.csv for train and test');
+        observer.next();
+        observer.complete();
+        // console.log(data[0], data.length)
+        // console.log(data[1], data.length)
+      })
+    });  
+  },
+  maximo: (xmls, dir, path) => {
+    return new Observable((observer) => {
+      let csv = 'filename,width,height,class,xmin,ymin,xmax,ymax\n';
+      xmls.forEach(async(name, i) => {
+        let xml = readFileSync(`${dir}/${name}`);
+        let xmljson = JSON.parse(convert.xml2json(xml, {compact: true, space: 2}));
+        // console.log(xmljson.annotation.object)
+        let object = xmljson.annotation.object;
+        let size = xmljson.annotation.size;  
+        try {
+          let json = jsonfile.readFileSync(`${path}/prop.json`);
+            if (json) {
+              let props = JSON.parse(json['file_prop_info']);
+              if(!Array.isArray(object)) {
+                object = [object];
+              }
+              object.forEach((obj) => {
+                let prop = props.filter((f) => f._id === obj.file_id._text);
+                if(prop.length != 1) {
+                  build.exit(`file mismatched or duplicated: ${prop[0]._id}, ${prop[0].original_file_name}`);
+                }
+                let bbox = obj.bndbox;
+                csv += `${prop[0].original_file_name},${size.width._text},${size.height._text},${obj.name._text},${bbox.xmin._text},${bbox.ymin._text},${bbox.xmax._text},${bbox.ymax._text}\n`;
+              })
+            } else {
+              build.exit(json);
+            }    
+        } catch(e) {
+          build.exit(`${name}: ${e}`)
+        }
+      })
+      observer.next(csv);
+      observer.complete();
+    });
+  },
+  build_all: () => {
+    return new Observable((observer) => {
+      const origin = process.env.npm_config_origin;
+      if(origin.toLowerCase() === 'maximo') {
+        build.rename_maximo_assets()
+        .subscribe(() => {
+          build.partition_dataset()
+          .subscribe(() => {
+            build.xml_to_csv()
+            .subscribe(() => {
+              observer.next();
+              observer.complete();
+            })
+          })
+        })
+      }
+    });
+  },
+  generate_tfrecord: () => {
+    return new Observable((observer) => {
+      const csv_input = process.env.npm_config_csv_input;
+      const image_dir = process.env.npm_config_image_dir;
+      const output_path = process.env.npm_config_output_path;
 
+      let arg = `python ${__dirname}/generate_tfrecord.py --csv_input=${csv_input} --image_dir=${image_dir} --output_path=${output_path}`;
+      exec(arg, {maxBuffer: 1024 * 2000}, (err, stdout, stderr) => {
+        if(!err) {
+          console.log(stdout)
+          console.log(`done generating ${output_path}`);
+          observer.next();
+          observer.complete();
+        } else {
+          console.log(`failed to generate ${output_path}`, err);
+        }
+      });
+    });  
   },
   exit: (msg) => {
     console.log(msg);
@@ -105,4 +188,5 @@ let build = {
   }
 }
 
-build[task]();
+build[task]()
+.subscribe(() => console.log('process completed.'));
